948da34bcdfef304b060b351c0e63e71c363a4e4
xdubra <xdubra05@vutbr.cz>
2021-03-20 16:19:55 +0100

    Lab6

--- 06-Driver for multiple seven-segment displays/Labs06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits1.vhd
+++ 06-Driver for multiple seven-segment displays/Labs06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits1.vhd
@@ -1,70 +1,3 @@
-# Digital-electronics-1
-https://github.com/xdubra/Digitalelectronics-1
-# Lab 6: Driver for multiple seven-segment displays
-## Timing diagram figure for displaying value 3.142
-
-![Screenshot od EDA Playground](image6/Prve.png)
-
-```vhdl
-{
-  signal:
-  [
-    ['Digit position',
-      {name: 'Common anode: AN(3)', wave: 'xx01..01..01'},
-      {name: 'AN(2)', wave: 'xx101..01..0'},
-      {name: 'AN(1)', wave: 'xx1.01..01..'},
-      {name: 'AN(0)', wave: 'xx1..01..01.'},
-    ],
-    ['Seven-segment data',
-      {name: '4-digit value to display', wave: 'xx3333555599', data: ['3','1','4','2','3','1','4','2','3','1']},
-      {name: 'Cathod A: CA', wave: 'xx01.0.1.0.1'},
-      {name: 'Cathod B: CB', wave: 'xx0.........'},
-      {name: 'Cathod C: CC', wave: 'xx0..10..10.'},
-      {name: 'Cathod D: CD', wave: 'xx01.0.1.0.1'},
-      {name: 'Cathod E: CE', wave: 'xx1..01..01.'},
-      {name: 'Cathod F: CF', wave: 'xx1.01..01..'},
-      {name: 'Cathod G: CG', wave: 'xx010..10..1'},
-    ],
-    {name: 'Decimal point: DP', wave: 'xx01..01..01'},
-  ],
-  head:
-  {
-    text: '                    4ms   4ms   4ms   4ms   4ms   4ms   4ms   4ms   4ms   4ms',
-  },
-}
-```
-## Display driver
-### Listing of VHDL code of the process ``` p_mux ```
-```vhdl
- p_mux : process(s_cnt, data0_i, data1_i, data2_i, data3_i, dp_i)
-    begin
-        case s_cnt is
-            when "11" =>
-                s_hex <= data3_i;
-                dp_o  <= dp_i(3);
-                dig_o <= "0111";
-
-            when "10" =>
-                s_hex <= data2_i;
-                dp_o  <= dp_i(2);
-                dig_o <= "1011";
-
-            when "01" =>
-                s_hex <= data1_i;
-                dp_o  <= dp_i(1);
-                dig_o <= "1101";
-
-            when others =>
-                s_hex <= data0_i;
-                dp_o  <= dp_i(0);
-                dig_o <= "1110";
-                
-        end case;
-    end process p_mux;
-```
-
-### Listing of VHDL testbench file ``` tb_driver_7seg_4digits ```
-```vhdl
 ------------------------------------------------------------------------
 --
 -- Template for 4-digit 7-segment display driver testbench.
@@ -173,71 +106,15 @@ begin
         
         s_dp_i   <=  "0111";
         
+        wait for 350 ns;
+        
+    --    s_data3  <=  "0001";
+    --    s_data2  <=  "0000";
+    --    s_data1  <=  "0001";
+    --    s_data0  <=  "0000";
         
         report "Stimulus process finished" severity note;
         wait;
     end process p_stimulus;    
 
 end architecture testbench;
-```
-
-### Screenshot with simulated time waveforms
-![Screenshot od EDA Playground](image6/Druhe.png)
-
-### Listing of VHDL architecture of the top layer
-```vhdl
-architecture Behavioral of top is
-   
-begin
-
-    --------------------------------------------------------------------
-    -- Instance (copy) of driver_7seg_4digits entity
-    driver_seg_4 : entity work.driver_7seg_4digits
-        port map(
-            clk        => CLK100MHZ,
-            reset      => BTNC,
-            data0_i(3) => SW(3),
-            data0_i(2) => SW(2),
-            data0_i(1) => SW(1),
-            data0_i(0) => SW(0),
-
-            data1_i(3) => SW(7),
-            data1_i(2) => SW(6),
-            data1_i(1) => SW(5),
-            data1_i(0) => SW(4),
-            
-            data2_i(3) => SW(11),
-            data2_i(2) => SW(10),
-            data2_i(1) => SW(9),
-            data2_i(0) => SW(8),
-            
-            data3_i(3) => SW(15),
-            data3_i(2) => SW(14),
-            data3_i(1) => SW(13),
-            data3_i(0) => SW(12),
-            
-            dp_i       => "0111",
-            dp_o       => DP,
-            
-            seg_o(6)   =>  CA,
-            seg_o(5)   =>  CB,
-            seg_o(4)   =>  CC,
-            seg_o(3)   =>  CD,
-            seg_o(2)   =>  CE,
-            seg_o(1)   =>  CF,
-            seg_o(0)   =>  CG,
-            
-            dig_o      =>  AN(4 - 1 downto 0)
-            
-        );
-
-    -- Disconnect the top four digits of the 7-segment display
-    AN(7 downto 4) <= b"1111";
-
-end architecture Behavioral;
-```
-
-## Eight-digit driver
-### Image of the driver schematic
-
-
